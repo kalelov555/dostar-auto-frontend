b58df8929639ff1e39c9f8de156be694d6b422ea
kalelov555 <sagynzhankalel@gmail.com>
2024-07-15 18:22:02 +0500

    configure prime react and icons

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,23 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import { Poppins } from "next/font/google";
+import { PrimeReactProvider } from "primereact/api";
+import "primeicons/primeicons.css";
+import "primereact/resources/themes/lara-light-indigo/theme.css";
+
+const roboto = Poppins({
+  weight: ["400", "600", "700", "900"],
+  style: ["normal", "italic"],
+  subsets: ["latin"],
+  display: "swap",
+});
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <div className={roboto.className}>
+      <PrimeReactProvider>
+        <Component {...pageProps} />
+      </PrimeReactProvider>
+    </div>
+  );
 }
