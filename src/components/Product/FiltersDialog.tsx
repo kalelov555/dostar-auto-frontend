a20ebba2f8877cee12ffeb558b04c68e03474190
kalelov555 <sagynzhankalel@gmail.com>
2024-07-18 15:47:34 +0500

    add filtration fields Initial Push

--- src/components/Product/FiltersDialog.tsx
+++ src/components/Product/FiltersDialog.tsx
@@ -1,23 +1,86 @@
+import { dataInputs } from "@/services/filters/product";
+import { Button } from "primereact/button";
 import { Dialog } from "primereact/dialog";
-import { Dispatch, SetStateAction } from "react";
+import { Dropdown } from "primereact/dropdown";
+import { InputText } from "primereact/inputtext";
+import { Dispatch, SetStateAction, useState } from "react";
+import { Controller, useForm } from "react-hook-form";
 
 type Props = {
   filtersModalOpened: boolean;
   setFiltersModalOpened: Dispatch<SetStateAction<boolean>>;
 };
 
+export interface IFilter {
+  name: string;
+  mark: string;
+  model: string;
+  priceFrom: string;
+  priceTo: string;
+  releaseDateFrom: string;
+  releaseDateTo: string;
+  transmission: string; //коробка передач
+  carcase: string; //кузов
+  fuel: string;
+  capacityFrom: string | null;
+  capacityTo: string | null;
+  wheelSide: "left" | "right" | ""; //left or right
+  driveUnit: string; // привод
+  color: string;
+  paymentType: "credit" | "instllment" | "";
+  crushState: "critical" | "notMoving" | "";
+  descriptionText: "";
+  sortBy: string;
+}
+
+type FilterObject = {
+  name: string;
+  value: string;
+};
+
 const FiltersDialog = ({
   filtersModalOpened,
   setFiltersModalOpened,
 }: Props) => {
+  const defaultValues: IFilter = {
+    name: "",
+    mark: "",
+    model: "",
+    priceFrom: "",
+    priceTo: "",
+    releaseDateFrom: "",
+    releaseDateTo: "",
+    transmission: "", //коробка передач
+    carcase: "",
+    fuel: "",
+    capacityFrom: "",
+    capacityTo: "",
+    wheelSide: "", //left or right
+    driveUnit: "", // привод
+    color: "",
+    paymentType: "",
+    crushState: "",
+    descriptionText: "",
+    sortBy: "",
+  };
+  const {
+    control,
+    formState: { errors },
+    handleSubmit,
+    reset,
+  } = useForm<IFilter>({ defaultValues });
+
+  const onSubmit = (data: IFilter) => {
+    alert(JSON.stringify(data));
+  };
+
   return (
     <Dialog
-      className="bg-[#f1f1f1]"
-      headerStyle={{ padding: "0" }}
+      className="bg-[#f1f1f1] overflow-scroll"
       id="filter"
       visible={filtersModalOpened}
       position={"center"}
-      style={{ maxWidth: "768px", width: "100%" }}
+      style={{ maxWidth: "768px", width: "100%", borderRadius: 0 }}
       onHide={() => {
         if (!filtersModalOpened) return;
         setFiltersModalOpened(false);
@@ -37,6 +100,82 @@ const FiltersDialog = ({
             ></i>
             <p className="text-base font-normal">Фильтр</p>
           </div>
+
+          <form
+            className="px-2 pt-0 pb-20 flex flex-col"
+            onSubmit={handleSubmit(onSubmit)}
+          >
+            {dataInputs.map((input) => (
+              <div
+                key={input.name}
+                className={`field ${input.floatingLabel ? "mt-8" : "mt-1"}`}
+              >
+                <span className={input.floatingLabel ? "p-float-label" : ""}>
+                  {!input.floatingLabel && (
+                    <label
+                      className="text-[12px] text-[#6b7280] mx-[0.75rem]"
+                      htmlFor={input.name}
+                    >
+                      {input.label}
+                    </label>
+                  )}
+                  {input.type === "select" ? (
+                    <Controller
+                      name={input.name}
+                      control={control}
+                      render={({ field }) => (
+                        <Dropdown
+                          id={field.name}
+                          value={field.value}
+                          onChange={(e) => field.onChange(e.value)}
+                          options={input.options}
+                          optionLabel="name"
+                          optionValue="value"
+                          placeholder={input.placeholder}
+                          className="w-full rounded-none"
+                          checkmark={true}
+                          name={input.name}
+                        />
+                      )}
+                    />
+                  ) : input.type === "range" ? (
+                    <div className="flex w-full items-center">
+                      {input.children?.map((child) => (
+                        <Controller
+                          key={child.name}
+                          name={child.name}
+                          control={control}
+                          render={({ field, fieldState }) => (
+                            <InputText
+                              id={field.name}
+                              placeholder={child.placeholder}
+                              keyfilter={child?.keyfilter}
+                              className="w-full"
+                              {...(field as any)}
+                            />
+                          )}
+                        />
+                      ))}
+                    </div>
+                  ) : (
+                    <></>
+                  )}
+                  {input.floatingLabel && (
+                    <label htmlFor={input.name}>{input.label}</label>
+                  )}
+                </span>
+              </div>
+            ))}
+
+            <div className="fixed bottom-4 max-w-[600px] w-full mx-auto self-center">
+              <Button
+                severity="info"
+                className="w-full h-12 shadow-[0_8px_25px_0_rgba(42,129,221,.6)]"
+                label="Поиск..."
+                type="submit"
+              />
+            </div>
+          </form>
         </div>
       }
     />
--- src/components/Product/TypesMenuDialog.tsx
+++ src/components/Product/TypesMenuDialog.tsx
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import { Dialog } from "primereact/dialog";
 import { Dispatch, SetStateAction } from "react";
 
@@ -6,9 +7,28 @@ type Props = {
   setTypeModalOpened: Dispatch<SetStateAction<boolean>>;
 };
 
+const items = [
+  {
+    pathname: "/products/cars",
+    label: "Лекговые",
+    icon: "pi pi-car",
+  },
+  {
+    pathname: "/products/commercial",
+    label: "Коммерческие",
+    icon: "pi pi-truck",
+  },
+  {
+    pathname: "/products/moto",
+    label: "Мото",
+    icon: "pi pi-car",
+  },
+];
+
 const TypesMenuDialog = ({ typeModalOpened, setTypeModalOpened }: Props) => {
   return (
     <Dialog
+      contentStyle={{ borderBottomLeftRadius: 0, borderBottomRightRadius: 0 }}
       id="type"
       header="Выберите Категорию"
       visible={typeModalOpened}
@@ -18,18 +38,43 @@ const TypesMenuDialog = ({ typeModalOpened, setTypeModalOpened }: Props) => {
         if (!typeModalOpened) return;
         setTypeModalOpened(false);
       }}
-      draggable={false}
-    >
-      <p className="m-0">
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
-        veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-        commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
-        velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
-        occaecat cupidatat non proident, sunt in culpa qui officia deserunt
-        mollit anim id est laborum.
-      </p>
-    </Dialog>
+      className="bg-[#f1f1f1]"
+      content={
+        <div style={{ padding: "1.5rem" }}>
+          <div className="flex w-full items-center justify-between">
+            <h1>Выберите Категорию</h1>
+            <i
+              onClick={() => setTypeModalOpened(false)}
+              className="pi pi-plus pi-pw rotate-45 text-2xl cursor-pointer"
+            ></i>
+          </div>
+          <ul className="text-lg mt-6">
+            {items.map((item) => (
+              <li
+                className="border-b-[0.5px] border-gray-primary first:pt-0 py-2 last:border-b-0"
+                key={item.pathname}
+                onClick={() => setTypeModalOpened(false)}
+              >
+                <Link
+                  className="flex justify-between items-center "
+                  href={{ pathname: item.pathname }}
+                >
+                  <div className="flex gap-3 items-center">
+                    <i
+                      className={`${item.icon} pi-pw text-lg text-primary`}
+                    ></i>
+                    <p>{item.label}</p>
+                  </div>
+                  <span>
+                    <i className="pi pi-chevron-right text-sm"></i>
+                  </span>
+                </Link>
+              </li>
+            ))}
+          </ul>
+        </div>
+      }
+    ></Dialog>
   );
 };
 
