1a34ba435795fe6e4fe10ca2d05e2f5ae5ae6aee
kalelov555 <sagynzhankalel@gmail.com>
2024-07-23 16:40:14 +0500

    init: add blog pages

---
+++ src/pages/blog/news/index.tsx
@@ -0,0 +1,12 @@
+import ProductPageFilters from "@/components/Product/ProductPageFilters";
+import DefaultLayout from "@/layouts/DefaultLayout";
+
+const ProductMotoPage = () => {
+  return (
+    <DefaultLayout>
+      <h1>Новости</h1>
+    </DefaultLayout>
+  );
+};
+
+export default ProductMotoPage;
