980952d79467097bd33af76430971bf122f27cc1
kalelov555 <sagynzhankalel@gmail.com>
2024-10-02 14:32:21 +0500

    refactor: remove ssr for deploying to gh pages

--- src/pages/products/commercial/spec_technics/[id].tsx
+++ src/pages/products/commercial/spec_technics/[id].tsx
@@ -1,53 +1,62 @@
 import ApplicationModal from "@/components/Applications/ApplicationModal";
 import ProductDescriptionTable from "@/components/Product/ProductDescriptionTable";
 import Gallery from "@/components/shared/Gallery";
+import LoadingScreen from "@/components/shared/LoadingScrenn";
 import { formatPrice } from "@/helpers/functions";
-import { GalleryImages } from "@/helpers/gallery";
 import { ISpecTechnic } from "@/interfaces/spec-technics";
 import DefaultLayout from "@/layouts/DefaultLayout";
 import api from "@/services/api/client";
-import { GetServerSidePropsContext } from "next";
-import ImageGallery from "react-image-gallery";
+import { useParams } from "next/navigation";
+import { useRouter } from "next/router";
+import { useEffect, useState } from "react";
 
-type ContextProps = {
-  data: ISpecTechnic;
-};
+const ProductCarPage = () => {
+  const [data, setData] = useState<ISpecTechnic>();
+  const [loading, setLoading] = useState(false);
+  const params = useParams();
+  const router = useRouter();
 
-const ProductCarPage = ({ data }: ContextProps) => {
+  useEffect(() => {
+    if (router.isReady) {
+      setLoading(true);
+      api
+        .get(`/spec_technics/${params.id}`)
+        .then((res) => setData(res.data.data))
+        .catch(() => router.replace("/404"))
+        .finally(() => setLoading(false));
+    }
+  }, [params, router]);
   return (
     <DefaultLayout>
-      <div className="bg-white">
-        <div className="w-full">
-          <Gallery items={data?.image_urls as string[]} />
-        </div>
-        <div className="flex items-center justify-between py-3 px-2">
-          <div className="flex flex-col">
-            <p className="text-lg font-semibold text-primary">
-              {data.manufacturer_name || ""} {data.model || ""}-{" "}
-              {data.manufacture_year} года
-            </p>
-            <h2 className="mt-1">{formatPrice(data.price)}</h2>
+      {loading || !data ? (
+        <LoadingScreen />
+      ) : (
+        <>
+          <div className="bg-white">
+            <div className="w-full">
+              <Gallery items={data?.image_urls as string[]} />
+            </div>
+            <div className="flex items-center justify-between py-3 px-2">
+              <div className="flex flex-col">
+                <p className="text-lg font-semibold text-primary">
+                  {data?.manufacturer_name || ""} {data?.model || ""}-{" "}
+                  {data?.manufacture_year} года
+                </p>
+                <h2 className="mt-1">{formatPrice(data?.price as number)}</h2>
+              </div>
+            </div>
+
+            <div></div>
           </div>
-        </div>
 
-        <div></div>
-      </div>
+          <div className="w-full border-y-[1px] border-gray-primary py-2 mt-4 bg-white">
+            <ProductDescriptionTable product={data} />
+          </div>
 
-      <div className="w-full border-y-[1px] border-gray-primary py-2 mt-4 bg-white">
-        <ProductDescriptionTable product={data} />
-      </div>
-
-      <ApplicationModal id={Number(data.id)} />
+          <ApplicationModal id={Number(data?.id)} />
+        </>
+      )}
     </DefaultLayout>
   );
 };
-
-export const getServerSideProps = async (
-  context: GetServerSidePropsContext
-) => {
-  const { id } = context.query;
-  const response = await api.get(`/spec_technics/${id}`);
-  return { props: { data: response.data.data } };
-};
-
 export default ProductCarPage;
